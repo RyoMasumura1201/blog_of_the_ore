59b0e878cab981ee779b62efcf40ac5d2d0e9873
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-09-25 09:37:19 +0900

    style: add syntax highlight

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,5 +1,5 @@
 import { CodeComponent } from 'react-markdown/lib/ast-to-react'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import SyntaxHighlighter  from 'react-syntax-highlighter';
 import { sunburst } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 const CodeBlock: CodeComponent = ({ inline, className, children }) => {
@@ -8,6 +8,7 @@ const CodeBlock: CodeComponent = ({ inline, className, children }) => {
   }
   const match = /language-(\w+)/.exec(className || '');
   const lang = match && match[1] ? match[1] : '';
+  
   return (
     <SyntaxHighlighter
       style={sunburst}
