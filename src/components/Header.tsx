a99df8be471aa7981dc02b32b20d7cd053f5b0b1
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-10-11 07:20:50 +0900

    style微調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,14 @@ export const Header: React.VFC<Props> = (props) => {
   return (
     <header>
       {home ? (
-        <HStack spacing='8' p='4' bg='black' color='white' justify='center'>
+        <HStack spacing='8' p='2' bg='black' color='white' justify='center'>
           <Image src='/images/profile.jpeg' alt={siteTitle} borderRadius='full' boxSize='50px' />
           <Text fontSize='xx-large' fontWeight='bold'>
             {siteTitle}
           </Text>
         </HStack>
       ) : (
-        <HStack spacing='8' p='4' bg='black' color='white' justify='center'>
+        <HStack spacing='8' p='2' bg='black' color='white' justify='center'>
           <Link href='/'>
             <a>
               <Image
