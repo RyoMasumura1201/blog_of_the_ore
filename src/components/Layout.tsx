09ad7f739a9c9d2107f0a0c8ebea70884284be20
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-10-11 06:54:14 +0900

    style: favicon変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,15 @@ export const Layout: VFC<Props> = ({ children, home }) => {
   return (
     <>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='icon'
+          href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👻</text></svg>'
+        />
+        <link
+          rel='icon alternate'
+          type='image/png'
+          href='https://twemoji.maxcdn.com/v/13.1.0/72x72/1f47b.png'
+        />
         <meta name='description' content='Learn how to build a personal website using Next.js' />
         <meta
           property='og:image'
