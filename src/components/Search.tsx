2d11b96411e6770c4021cef35566023adfe8cc2b
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-10-15 08:26:04 +0900

    fix: onFocusによるinputの再レンダリングを防止

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,36 +2,17 @@ import algoliasearch from 'algoliasearch/lite';
 import { Hits, Highlight, InstantSearch, connectSearchBox } from 'react-instantsearch-dom';
 import Link from 'next/link';
 import { Box } from '@chakra-ui/layout';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
+import CustomSearchBox from './CustomSearchBox';
 
 export const Search: React.VFC = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   const searchClient = algoliasearch(
     process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
     process.env.NEXT_PUBLIC_ALGOLIA_API_KEY,
   );
   const indexName = 'blog_of_the_ryo';
-
-  const SearchBox = ({ currentRefinement, refine }: any) => {
-    return (
-      <Box w='100%' m='auto'>
-        <InputGroup>
-          <Input
-            placeholder='記事を検索'
-            value={currentRefinement}
-            onChange={(e) => refine(e.currentTarget.value)}
-          />
-          <InputLeftElement>
-            <SearchIcon color='gray.700' />
-          </InputLeftElement>
-        </InputGroup>
-      </Box>
-    );
-  };
-
-  const CustomSearchBox = connectSearchBox(SearchBox);
 
   const HitBlock = ({ hit }: any) => {
     return (
