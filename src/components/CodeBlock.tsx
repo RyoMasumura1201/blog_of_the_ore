0ce58ca0264773632422b0ba9139b3dc306dba13
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-10-17 13:47:22 +0900

    style: codeblockにファイル名追加

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,18 +1,33 @@
 import { CodeComponent } from 'react-markdown/lib/ast-to-react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { sunburst } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import { Box } from '@chakra-ui/layout';
 
 const CodeBlock: CodeComponent = ({ inline, className, children }) => {
   if (inline) {
     return <code className={className}>{children}</code>;
   }
-  const match = /language-(\w+)/.exec(className || '');
+  const match = /language-(\w+)(:.+)/.exec(className || '');
   const lang = match && match[1] ? match[1] : '';
+  const name = match && match[2] ? match[2].slice(1) : '';
 
   return (
-    <SyntaxHighlighter style={sunburst} language={lang}>
-      {String(children).replace(/\n$/, '')}
-    </SyntaxHighlighter>
+    <>
+      {name ? (
+        <Box position='relative'>
+          <Box display='inline-block' position='absolute' bg='gray.100'>
+            {name}
+          </Box>
+          <SyntaxHighlighter style={sunburst} language={lang}>
+            {'\n' + String(children).replace(/\n$/, '')}
+          </SyntaxHighlighter>
+        </Box>
+      ) : (
+        <SyntaxHighlighter style={sunburst} language={lang}>
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      )}
+    </>
   );
 };
 
