1cbb49ccb8ca4f4b0c482f2f8a18ec1fb8642384
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-10-18 06:57:39 +0900

    style: footerのサイズ変更によるpaddingの調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,15 @@ import { FaGithub } from 'react-icons/fa';
 export const Footer: React.VFC = () => {
   return (
     <footer>
-      <Box bg='black' width='100%' p='4' textAlign='center'>
-        <a href='https://github.com/RyoMasumura1201/blog_of_the_ore'>
-          <Center>
-            <FaGithub color='white' size='50' />
-          </Center>
-          <Text color='gray.100'>Source Code is here</Text>
-        </a>
+      <Box bg='black' width='100%' p='2' textAlign='center'>
+        <Center>
+          <a href='https://github.com/RyoMasumura1201/blog_of_the_ore'>
+            <Center>
+              <FaGithub color='white' size='50' />
+            </Center>
+            <Text color='gray.100'>Source Code is here</Text>
+          </a>
+        </Center>
       </Box>
     </footer>
   );
