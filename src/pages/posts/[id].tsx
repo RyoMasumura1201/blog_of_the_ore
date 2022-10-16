42dc22156cbf5b7e0fc9658e5f017bc6bed7ec6a
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-10-16 17:03:56 +0900

    画像はnext/imageで最適化するように

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,13 +1,20 @@
-import { Layout } from '../../components/Layout';
-import { getAllPostIds, getPostData } from 'lib/posts';
+import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import Date from '../../components/Date';
+import Image from 'next/image';
 import { Text } from '@chakra-ui/react';
 import { Box } from '@chakra-ui/layout';
+import { unified } from 'unified';
+import rehypeParse from 'rehype-parse';
+import rehypeReact from 'rehype-react';
+import type { Options as RehypeReactOptions } from 'rehype-react';
+import 'prismjs/themes/prism-tomorrow.css';
+
+import { getAllPostIds, getPostData } from 'lib/posts';
+import { Layout } from '../../components/Layout';
+import Date from '../../components/Date';
 import { postDataType } from 'type';
 import { SEO } from '@/components/SEO';
 import markdownStyles from '../../../styles/markdown.module.css';
-import 'prismjs/themes/prism-tomorrow.css';
 
 interface postPage extends postDataType {
   html: string;
@@ -17,8 +24,46 @@ type Props = {
   postData: postPage;
 };
 
+type ImageElementProps = {
+  src: string;
+  alt: string;
+};
+
 export default function Post(props: Props) {
   const { postData } = props;
+
+  const ImageElement: React.VFC<ImageElementProps> = (props) => {
+    const { src, alt } = props;
+    return (
+      <picture>
+        <Box style={{ position: 'relative', minHeight: '20em', width: '100%' }}>
+          <Image
+            src={require(`../../../posts/${postData.id}/${src}`)}
+            alt={alt}
+            layout='fill'
+            objectFit='contain'
+          />
+        </Box>
+      </picture>
+    );
+  };
+
+  const RehypeReact = (html: string) => {
+    const result = unified()
+      .use(rehypeParse, {
+        fragment: true,
+      })
+      .use(rehypeReact, {
+        createElement: React.createElement,
+        components: {
+          img: (props: ImageElementProps) => {
+            return ImageElement(props);
+          },
+        },
+      } as RehypeReactOptions)
+      .processSync(html);
+    return result.result;
+  };
 
   return (
     <Layout>
@@ -37,10 +82,7 @@ export default function Post(props: Props) {
           <Date dateString={postData.date} />
         </Box>
         <Box paddingLeft={{ base: '10', md: '40' }} paddingRight={{ base: '10', md: '40' }}>
-          <div
-            className={markdownStyles.body}
-            dangerouslySetInnerHTML={{ __html: postData.html }}
-          />
+          <div className={markdownStyles.body}>{RehypeReact(postData.html)}</div>
         </Box>
       </article>
     </Layout>
